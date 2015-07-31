d0995ec799c92bf4ba7a8cc4d84b5ffee22008b0
Pa-Paolo <paolobono98@gmail.com>
2015-08-01 00:07:16 +0200

    commentato un metodo

--- src/bot/VideoUploader.java
+++ src/bot/VideoUploader.java
@@ -2,6 +2,12 @@ package bot;
 
 public class VideoUploader
 	{
+		/**
+		 * Carica un video data la posizione assoluta
+		 * @param message
+		 * @param path
+		 * @return true se il file è stato uploadato
+		 */
 		public static boolean upload(Message message, String path)
 		{
 			return bot.ExternalExecuter.executeCmd("curl -s -X POST \"https://api.telegram.org/bot" + Main.getIdCode() + "/sendVideo\" -F chat_id=46365292 -F video=\"@" + path + "\"", "ytLog");
